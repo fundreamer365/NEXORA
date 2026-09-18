ed1b6d2a7a429935306dea951b7b6f7f80005eb1
FunDreamer <fundreamer365@gmail.com>
2026-09-18 23:13:05 +0300

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -2,5 +2,5 @@
 // ВНИМАНИЕ: используем ТОЛЬКО publishable (anon) ключ.
 // НИКОГДА не вставляй сюда service_role key.
 
-export const SUPABASE_URL = "https://jylukilnkgkvmjnierqy.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_U5rKld1LYukIaFZecnYNaA_cgaJZu4R";
+export const SUPABASE_URL = "----ааааа";
+export const SUPABASE_ANON_KEY = "ааа-а-а-а-а--а-а";
